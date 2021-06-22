7ea4c49774447ca25d33a032e462613fa24a0154
Samir Mejia <samirmejiaat@unimagdalena.edu.co>
2021-06-22 14:17:16 -0500

    Creand vista para Registro

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import gif from '../../assets/mobilegif.gif';
+import logo from '../../assets/logo.svg';
+
+import { Container, Gif, FormContainer, Form, Footer } from './styles';
 
 const SignUp = () => {
-    return <h1>SignUp</h1> 
+    const [input, setInput] = useState('');
+
+    return (
+        <Container>
+            <Gif src={gif} alt="gif" />
+
+            <FormContainer>
+                <Form>
+                    <img src={logo} alt="photogram" width='100%' />
+                    <span>Registrate para ver fotos</span>
+
+                    <hr/>
+
+                    <input value={input} name="name" onChange={(e) => setInput(e.target.value)} />
+                    
+                    <button type="submit">Registrate</button>
+
+                    <hr/>
+
+                    <span className="footer">
+                        Al registrate, aceptas nuestras condiciones
+                    </span>
+
+                    <Footer>
+                        <p>
+                            ¿Tienes una cuenta? <Link to="signup">Entrar</Link>
+                        </p>
+                    </Footer>
+
+                </Form>
+            </FormContainer>
+        </Container>
+    )
 }
+
 export default SignUp;
